598d85f402b48e4c28a782db37e2f4934ff52f76
AbuBakkarSiddiqueAnir <anirdms@gamil.com>
2023-02-16 10:33:05 +0600

    basic layout outlined

---
+++ src/Layout/main.tsx
@@ -0,0 +1,14 @@
+
+
+import React, { ReactHTMLElement } from 'react'
+import './main.css'
+
+const  Main= ({children}:any) => {
+
+  return ( <div className='container'>
+    {children}
+  </div>)
+}
+
+export default Main
+
