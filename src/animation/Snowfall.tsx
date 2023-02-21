3acd9ad7fd7ee4a5cd98ebc5c9e17296a677090a
AbuBakkarSiddiqueAnir <anirdms@gamil.com>
2023-02-21 13:01:02 +0600

    snowfall added

---
+++ src/animation/Snowfall.tsx
@@ -0,0 +1,18 @@
+import './Snowfall.css'
+type TypeName= {
+
+};
+
+const  Snowfall:React.FC<TypeName> = () => {
+
+    return (
+        <section className='container-main'>
+          <div className='snow'>
+
+          </div>
+
+        </section>
+    )
+}
+
+export default Snowfall
