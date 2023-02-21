1ebb747242b7f01a360252d2d3b3c97727467eee
AbuBakkarSiddiqueAnir <anirdms@gamil.com>
2023-02-21 22:48:46 +0600

    animation changed for snowfall that must be refactored

--- src/animation/snowfall/Snowfall.tsx
+++ src/animation/snowfall/Snowfall.tsx
@@ -1,18 +1,95 @@
-import './Snowfall.css'
-type TypeName= {
+import './Snowfall.scss'
+type TypeName = {
 
 };
 
-const  Snowfall:React.FC<TypeName> = () => {
+const Snowfall: React.FC<TypeName> = () => {
 
-    return (
-        <section className='container-main'>
-          <div className='snow'>
+  return (
+    <section className='container-main'>
+      <div className='cloud1'>
+        <div className="cloud1__face"><span></span><span></span></div>
+        <div className="cloud1__clouds1">
+          <span></span>
+        </div>
+        <div className="cloud1__clouds2">
+          <span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud1__clouds3">
+          <span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud1__clouds4">
+          <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud1__clouds5">
+          <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud1__rain">
+          <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud1__lightning">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
 
-          </div>
+      <div className='cloud2'>
+        <div className="cloud1__face"><span></span><span></span></div>
+        <div className="cloud2__clouds1">
+          <span></span>
+        </div>
+        <div className="cloud2__clouds2">
+          <span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud2__clouds3">
+          <span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud2__clouds4">
+          <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud2__clouds5">
+          <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud2__rain">
+          <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud2__lightning">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
 
-        </section>
-    )
+      <div className='cloud3'>
+        <div className="cloud1__face"><span></span><span></span></div>
+        <div className="cloud3__clouds1">
+          <span></span>
+        </div>
+        <div className="cloud3__clouds2">
+          <span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud3__clouds3">
+          <span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud3__clouds4">
+          <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud3__clouds5">
+          <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud3__rain">
+          <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+        </div>
+        <div className="cloud3__lightning">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
+    </section>
+  )
 }
 
 export default Snowfall
